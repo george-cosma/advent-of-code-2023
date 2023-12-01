c3f773980cd587fcd39903a5da91db22c03b7027
Cosma George <georgemagic10@gmail.com>
2023-12-01 13:27:31 +0200

    Day 1 part 2

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -1,6 +1,44 @@
-use std::{fs, io::{BufReader, BufRead}, env};
-fn main() -> Result<(), anyhow::Error>{
+use std::{
+    env, fs,
+    io::{BufRead, BufReader},
+};
 
+fn str_to_digit(buf: &str) -> Option<u32> {
+    let simple_digit = match buf.chars().next() {
+        Some('0') => Some(0),
+        Some('1') => Some(1),
+        Some('2') => Some(2),
+        Some('3') => Some(3),
+        Some('4') => Some(4),
+        Some('5') => Some(5),
+        Some('6') => Some(6),
+        Some('7') => Some(7),
+        Some('8') => Some(8),
+        Some('9') => Some(9),
+        Some(_) => None,
+        None => None,
+    };
+
+    if simple_digit.is_some() {
+        return simple_digit;
+    }
+
+    match buf {
+        s if s.starts_with("zero") => Some(0),
+        s if s.starts_with("one") => Some(1),
+        s if s.starts_with("two") => Some(2),
+        s if s.starts_with("three") => Some(3),
+        s if s.starts_with("four") => Some(4),
+        s if s.starts_with("five") => Some(5),
+        s if s.starts_with("six") => Some(6),
+        s if s.starts_with("seven") => Some(7),
+        s if s.starts_with("eight") => Some(8),
+        s if s.starts_with("nine") => Some(9),
+        _ => None,
+    }
+}
+
+fn main() -> Result<(), anyhow::Error> {
     if env::args().len() != 2 {
         anyhow::bail!("You must pass the input file as a parameter! Example:\nday1 ./input.txt")
     }
@@ -21,8 +59,8 @@ fn main() -> Result<(), anyhow::Error>{
                 let mut first_digit: Option<u32> = None;
                 let mut last_digit: Option<u32> = None;
 
-                for c in buffer.chars() {
-                    if let Some(val) = c.to_digit(10) {
+                for i in 0..buffer.len() {
+                    if let Some(val) = str_to_digit(&buffer[i..]) {
                         if first_digit == None {
                             first_digit = Some(val);
                         } else {
@@ -42,12 +80,12 @@ fn main() -> Result<(), anyhow::Error>{
                 // println!("{first_digit:?} {last_digit:?}");
 
                 sum += first_digit.unwrap() * 10 + last_digit.unwrap();
-            },
+            }
         }
-        
+
         buffer.clear();
     }
-    
+
     println!("Sum found: {}", sum);
 
     Ok(())
